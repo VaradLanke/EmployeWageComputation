5b5bfa9b736ef1d23ed6a8a1475aa09231b3c261
VaradLanke <lanke.varad@gmail.com>
2020-05-12 14:30:40 +0530

    <Add> : Monthly Wage Calculation of One employee

--- src/com/employeewage/solution/service/EmployeeWageCompute.java
+++ src/com/employeewage/solution/service/EmployeeWageCompute.java
@@ -1,8 +1,19 @@
 package com.employeewage.solution.service;
 import java.util.InputMismatchException;
 import com.employeewage.solution.model.Employee;
+import java.util.Random;
 
 public class EmployeeWageCompute{
+
+
+	public boolean isPresent(Employee emp){
+		/*int randomNumber = sysRand.nextInt(100)%2;
+		return randomNumber==1 ? true : false; */
+		if(emp.getWorkHour() < 1){
+			return false;
+		}
+		return true;
+	}
 
 	public int calculatePay(Employee emp){
 		if(emp.getWorkHour() < emp.getFullDayHour()){
@@ -15,58 +26,13 @@ public class EmployeeWageCompute{
 		return emp.getPay();
 	}
 
-	public boolean isPresent(Employee emp){
-		/*int randomNumber = sysRand.nextInt(100)%2;
-		return randomNumber==1 ? true : false; */
-		if(emp.getWorkHour() < 1){
-			return false;
+	public int monthlyCalculatePay(Employee emp){
+		Random sysRand = new Random();
+		for(int i=0;i<20;i++){
+			emp.setWorkHour(sysRand.nextInt(9));
+			emp.setPay(emp.getPay() + (emp.getDailyWagePerHour() * emp.getWorkHour()));
 		}
-		return true;
+		return emp.getPay();
 	}
 
-	/*public static int wageMenu(){
-		System.out.println("1.Calculate-Pay");
-		System.out.println("2.Print Employee-Type");
-		System.out.println("3.Print Present/Absent");
-		System.out.println("4.Exit");
-		System.out.println("Enter Choice : ");
-		int choice = sc.nextInt();
-		System.out.println();
-		return choice;	
-	}*/
-
-	/*public static void main(String[] args){
-		try{
-			System.out.println("Welcome to Employee Wage Computation Program");
-			Employee empObj = new EmployeeWageCompute(20,sysRand.nextInt(9),8);
-
-			while(true){
-				int option = wageMenu();
-				switch(option){
-					case 1:
-							System.out.println("Pay  : " + computeObj.calculatePay());
-							break;
-					case 2:
-							System.out.println("Type : " + computeObj.getTypeEmployee());
-							break;
-					case 3:
-							if(computeObj.isPresent()){
-								System.out.println("Present");
-							}else{
-								System.out.println("Absent");
-							}
-							break;
-					case 4:
-							System.exit(0);
-							break;
-					default:
-						System.out.println("Invalid Choice!!");
-						break;
-					}
-
-				}
-			}catch(InputMismatchException e){
-				System.out.println("Wrong Input by User, expected number");
-		}
-	}*/
 }
