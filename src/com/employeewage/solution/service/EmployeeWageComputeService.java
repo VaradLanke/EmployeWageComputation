76ba935615a6696fced7fb75fd1c4c0367d6424a
VaradLanke <lanke.varad@gmail.com>
2020-05-14 21:29:13 +0530

    <Add> : Interface implementation on Service

--- src/com/employeewage/solution/service/EmployeeWageComputeService.java
+++ src/com/employeewage/solution/service/EmployeeWageComputeService.java
@@ -1,9 +1,14 @@
 package com.employeewage.solution.service;
+
+import com.employeewage.solution.model.Company;
 import com.employeewage.solution.model.Employee;
 
 public interface EmployeeWageComputeService{
 
 	public boolean isPresent(Employee emp);
+	public int generateRandomWorkHours(String companyName);
+	public void addCompanyWithEmployees(String companyName,int empRatePerHour,int numOfWorkingDays,int maxHoursPerMonth);
+	public Company findCompany(String companyName);
 	/*public int calculatePay(Employee emp);
 	public String getTypeOfEmployee(Employee emp);
 	public int monthlyCalculatePay(Employee emp);
