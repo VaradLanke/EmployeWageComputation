701366d3983dca21bb7ea8be939c55995f8b72bf
VaradLanke <lanke.varad@gmail.com>
2020-05-14 21:14:58 +0530

    <Refactor> : ArrayList of Companies

--- src/com/employeewage/solution/service/EmployeeWageCompute.java
+++ src/com/employeewage/solution/service/EmployeeWageCompute.java
@@ -1,7 +1,6 @@
 package com.employeewage.solution.service;
 import java.util.*;
 import com.employeewage.solution.model.*;
-import com.employeewage.solution.service.*;
 
 public class EmployeeWageCompute implements EmployeeWageComputeService{
 
@@ -46,47 +45,4 @@ public class EmployeeWageCompute implements EmployeeWageComputeService{
 		}
 		return null;
 	}
-/*
-	@Override
-	public int calculatePay(Employee emp){
-		emp.setPay(0);
-		emp.setPay(dailyWagePerHour * emp.getWorkHour());
-		return emp.getPay();
-	}
-
-	@Override
-	public String getTypeOfEmployee(Employee emp){
-		if((emp.getWorkHour()/20) < fullDayHour){
-			emp.setTypeEmployee("Part-Time");
-		}
-		else{
-			emp.setTypeEmployee("Full-Time");
-		}
-		return emp.getTypeEmployee();		
-	}
-
-	@Override
-	public int monthlyCalculatePay(Employee emp){
-		Random sysRand = new Random();
-		emp.setPay(0);
-		emp.setWorkHour(0);
-		for(int i=0;i<20;i++){
-			emp.setWorkHour(emp.getWorkHour() + sysRand.nextInt(9));
-		}
-		emp.setPay(dailyWagePerHour * emp.getWorkHour());
-		return emp.getPay();
-	}
-
-	@Override
-	public int tillHundredCalculatePay(Employee emp){
-		Random sysRand = new Random();
-		emp.setPay(0);
-		emp.setWorkHour(0);
-		while(emp.getWorkHour() <= 100){
-			emp.setWorkHour(emp.getWorkHour() + sysRand.nextInt(9));
-		}
-		emp.setPay(dailyWagePerHour * emp.getWorkHour());
-		return emp.getPay();
-	}
-*/
 }
